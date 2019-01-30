9142c02687d9fa44c7544ab450d442992975e7f2
Mayuri Rathod <mayuri.gr@gmail.com>
2019-01-30 11:37:22 +0800

    changes dispatch n subscribe

--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
 import { Provider } from 'react-redux';
 import {createStore} from 'redux';
-import {incrementNumber} from './actions';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {incrementNumber} from './actions';
+import rootReducer from './reducers/index';
+
+const store = createStore(rootReducer);
+
+store.subscribe(()=>console.log('store',store.getState()));
+
+store.dispatch(incrementNumber(store.getState()));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,12 @@
 import {INC_NUMBER} from '../actions';
 
-export const performAction = (state,action)=>{
+export default function performAction  (state={num:0},action){
     switch(action.type){
         case INC_NUMBER:
-        console.log('Action performed from the reducer');
+        console.log('Action performed from the reducer ->state',state,'action ->',action);
         let num=0;
-        return num;
-        default:
-        num=0;
-        return num;
+        return {num};
+        default:  
+        return state;
     }
 }
